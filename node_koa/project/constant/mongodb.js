3e7b0964b2db61058402e07365d794a65c091515
rongW <15871494583@163.com>
2019-02-17 15:49:52 +0800

    掩藏数据库用户名和密码
    链接你的个人数据库用户密码即可

--- node_koa/project/constant/mongodb.js
+++ node_koa/project/constant/mongodb.js
@@ -1,3 +1,3 @@
 module.exports={
-    mongoURI:'mongodb+srv://rong_log_table:wurong0608@cluster0-7aysj.mongodb.net/test?retryWrites=true'
+    mongoURI:'mongodb+srv://<dbusername>:<dbpassword>@cluster0-7aysj.mongodb.net/test?retryWrites=true'
 }
